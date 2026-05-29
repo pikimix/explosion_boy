b52b295965dfd6766fc17cfa5cf85d0f756b8363
Keith Summers <gamedev@pikimix.uk>
2026-05-29 14:16:05 +0100

    make widget clearer

--- app/ui/volume_widget.py
+++ app/ui/volume_widget.py
@@ -16,7 +16,7 @@ def draw(volume: float) -> None:
     bar_cx = bar_left + _BAR_W / 2
     bar_y = _PADDING + _BAR_H / 2
 
-    label_h = 16
+    label_h = 28
     bg_pad = 6
     bg_w = _BAR_W + bg_pad * 2
     bg_h = _BAR_H + label_h + 4 + bg_pad * 2
@@ -42,10 +42,18 @@ def draw(volume: float) -> None:
         border_width=1,
     )
     arcade.draw_text(
-        f'\U0001f50a {int(volume * 100)}%  [ ]',
-        bar_cx, bar_y + _BAR_H / 2 + 4,
+        f'\U0001f50a Volume: {int(volume * 100)}%',
+        bar_cx, bar_y + _BAR_H / 2 + 14,
         color=(220, 220, 220, 200),
         font_size=10,
+        anchor_x='center',
+        anchor_y='bottom',
+    )
+    arcade.draw_text(
+        'press [ to lower, ] to raise',
+        bar_cx, bar_y + _BAR_H / 2 + 2,
+        color=(160, 160, 160, 180),
+        font_size=8,
         anchor_x='center',
         anchor_y='bottom',
     )
